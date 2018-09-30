d0701bcafb4b39b3ce7f1165281d829996481144
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2018-09-30 20:34:43 +0200

    Create main.pp

---
+++ main.pp
@@ -0,0 +1,30 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+procedure main
+begin
+Object Pascal ist scheiße;
+end;
+
+{$R *.lfm}
+
+end.    
